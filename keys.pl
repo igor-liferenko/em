120847013f2faf42ec8a1558521d67b71aae1b98
Igor Liferenko <igor.liferenko@gmail.com>
2024-04-26 19:52:08 +0700

    edit

--- keys.pl
+++ keys.pl
@@ -1,66 +1,79 @@
-use strict;
+sub prnt ($)
+{
+  print "\e[2J";
+  print "\e[1;1f";
+  print shift;
+}
+
 $|++;
 
-binmode(STDIN, ':utf8');
-binmode(STDOUT, ':utf8');
-
 my @keys = (
-  { desc => "F1", bytes => "\eOP" },
-  { desc => "F2", bytes => "\eOQ" },
-  { desc => "up", bytes => "\e[A" },
-  { desc => "down", bytes => "\e[B" },
-  { desc => "right", bytes => "\e[C" },
-  { desc => "left", bytes => "\e[D" }
+  [ "F1",                      "\eOP"  ],
+  [ "F2",                      "\eOQ"  ],
+  [ "up previous-line",        "\e[A"  ],
+  [ "down next-line",          "\e[B"  ],
+  [ "left backward-character", "\e[D"  ],
+  [ "right forward-character", "\e[C"  ],
+  [ "home beginning-of-line",  "\e[H"  ],
+  [ "end end-of-line",         "\e[F"  ],
+  [ "DEL forward-delete-char", "\e[3~" ],
+  [ "PgUp",                    "\e[5~" ],
+  [ "PgDn",                    "\e[6~" ]
 );
 
 my @buffer = ();
 my $done = 0;
 KEY:
 while (!$done) {
-  # if recorded bytes remain, return next recorded byte
+  # if recorded bytes remain, handle next recorded byte
   if (scalar @buffer) {
-    my $b = shift @buffer;
-    my $n = ord($b);
-    if (($n >= 32 && $n < 127) || $n > 127) { print $b }
-    elsif ( $n == 8 ) { print "BS" }
-    elsif ( $n == 9 ) { print "TAB" }
-    elsif ( $n == 13 ) { print "LF" }
-    elsif ( $n == 26 ) { $done = 1 } # ^Z
-    elsif ( $n == 27 ) { print "ESC" }
-    else { print "NOT BOUND" }
+    my $k = shift @buffer;
+    if (ord($k) == ord('A') - 0100) { prnt 'beginning_of_line' }
+    elsif (ord($k) == ord('B') - 0100) { prnt 'left' }
+    elsif (ord($k) == ord('D') - 0100) { prnt 'delete' }
+    elsif (ord($k) == ord('E') - 0100) { prnt 'end_of_line' }
+    elsif (ord($k) == ord('F') - 0100) { prnt 'right' }
+    elsif ($k eq "\b") { prnt "BS" }
+    elsif ($k eq "\t") { prnt "TAB" }
+    elsif ($k eq "\r") { prnt "RETURN" }
+    elsif (ord($k) == ord('N') - 0100) { prnt 'down' }
+    elsif (ord($k) == ord('P') - 0100) { prnt 'up' }
+    elsif (ord($k) == ord('V') - 0100) { prnt 'pgdown' }
+    elsif (ord($k) == ord('Z') - 0100) { $done = 1 }
+    elsif ($k eq "\e") { prnt "ESC" }
+    elsif (ord($k) >= 32 && ord($k) != 127) { prnt $k }
+    else { prnt 'NOT BOUND' }
     next KEY;
   }
   my $submatch;
   do {
-    my $key = '';
-    if (scalar @buffer) {
+    my $k = '';
+    if (scalar(@buffer) == 1) { # to use ^[
       eval {
         local $SIG{ALRM} = sub { die }; 
-        alarm 1; # ualarm 300000; # 0.3 seconds
-        read(STDIN, $key, 1);
-        alarm 0; # ualarm 0;
+        ualarm 300_000;
+        read(STDIN, $k, 1);
+        ualarm 0;
       };
       next KEY if $@;
     }
-    else { read(STDIN, $key, 1) }
-    push @buffer, $key;
+    else { read(STDIN, $k, 1) }
+    push @buffer, $k;
     $submatch = 0;           
-    for (my $k = 0; $k <= $#keys; $k++) {
+    for my $key (@keys) {
       my $i = 0;
       while (1) {
-        last if $i > scalar(@buffer) || $i > length($keys[$k]{bytes});
-        if ($i == scalar(@buffer) && $i == length($keys[$k]{bytes})) {
+        last if $i > scalar(@buffer) || $i > length($$key[1]);
+        if ($i == scalar(@buffer) && $i == length($$key[1])) {
           @buffer = ();
-          print "\e[2J";
-          print "\e[1;1f";
-          print $keys[$k]{desc};
+          prnt $$key[0];
           next KEY;
         }
-        last if $i == scalar(@buffer) || $i == length($keys[$k]{bytes});
-        last if $buffer[$i] ne substr($keys[$k]{bytes}, $i, 1);
+        last if $i == scalar(@buffer) || $i == length($$key[1]);
+        last if $buffer[$i] ne substr($$key[1], $i, 1);
         $i++;
       }
-      $submatch = 1 if $i == scalar(@buffer) && $i != length($keys[$k]{bytes});
+      $submatch = 1 if $i == scalar(@buffer) && $i != length($$key[1]);
     }
   } while ($submatch);
 }
