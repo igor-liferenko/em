034edf2b2d4691f492a5cd3f87fc0404ff2681f8
Igor Liferenko <igor.liferenko@gmail.com>
2024-04-26 19:58:22 +0700

    edit

--- keys.pl
+++ keys.pl
@@ -27,22 +27,28 @@ KEY:
 while (!$done) {
   # if recorded bytes remain, handle next recorded byte
   if (scalar @buffer) {
-    my $k = shift @buffer;
-    if (ord($k) == ord('A') - 0100) { prnt 'beginning_of_line' }
-    elsif (ord($k) == ord('B') - 0100) { prnt 'left' }
-    elsif (ord($k) == ord('D') - 0100) { prnt 'delete' }
-    elsif (ord($k) == ord('E') - 0100) { prnt 'end_of_line' }
-    elsif (ord($k) == ord('F') - 0100) { prnt 'right' }
-    elsif ($k eq "\b") { prnt "BS" }
-    elsif ($k eq "\t") { prnt "TAB" }
-    elsif ($k eq "\r") { prnt "RETURN" }
-    elsif (ord($k) == ord('N') - 0100) { prnt 'down' }
-    elsif (ord($k) == ord('P') - 0100) { prnt 'up' }
-    elsif (ord($k) == ord('V') - 0100) { prnt 'pgdown' }
-    elsif (ord($k) == ord('Z') - 0100) { $done = 1 }
-    elsif ($k eq "\e") { prnt "ESC" }
-    elsif (ord($k) >= 32 && ord($k) != 127) { prnt $k }
-    else { prnt 'NOT BOUND' }
+    if (scalar(@buffer) > 1) {
+      prnt 'NOT BOUND';
+      @buffer = ();
+    }
+    else {
+      my $k = shift @buffer;
+      if (ord($k) == ord('A') - 0100) { prnt 'beginning_of_line' }
+      elsif (ord($k) == ord('B') - 0100) { prnt 'left' }
+      elsif (ord($k) == ord('D') - 0100) { prnt 'delete' }
+      elsif (ord($k) == ord('E') - 0100) { prnt 'end_of_line' }
+      elsif (ord($k) == ord('F') - 0100) { prnt 'right' }
+      elsif ($k eq "\b") { prnt "BS" }
+      elsif ($k eq "\t") { prnt "TAB" }
+      elsif ($k eq "\r") { prnt "RETURN" }
+      elsif (ord($k) == ord('N') - 0100) { prnt 'down' }
+      elsif (ord($k) == ord('P') - 0100) { prnt 'up' }
+      elsif (ord($k) == ord('V') - 0100) { prnt 'pgdown' }
+      elsif (ord($k) == ord('Z') - 0100) { $done = 1 }
+      elsif ($k eq "\e") { prnt "ESC" }
+      elsif (ord($k) >= 32 && ord($k) != 127) { prnt $k }
+      else { prnt 'NOT BOUND' }
+    }
     next KEY;
   }
   my $submatch;
